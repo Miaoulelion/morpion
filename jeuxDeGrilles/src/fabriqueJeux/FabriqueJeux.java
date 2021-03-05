f3c423572531707d122b41ef20e596ecb914f47e
MiaouLeLion <nicolas-berthier@orange.fr>
2021-03-05 15:46:25 +0100

    amelioration main

--- jeuxDeGrilles/src/fabriqueJeux/FabriqueJeux.java
+++ jeuxDeGrilles/src/fabriqueJeux/FabriqueJeux.java
@@ -7,6 +7,9 @@ import jeuxDeGrilles.Puissance4;
 public class FabriqueJeux {
 	
 	public static Jeux FabriqueJeu(String [] arg) {
+		if(arg.length<1) {
+			throw new IllegalArgumentException("Un argument est indispensable à l'exécution.");
+		}
 		int choix=Integer.parseInt(arg[0]);
 		if(choix==1) {
 			return new Morpion();
